28090382047b5a978b04182e7c55e3a16a39d985
linyoujie <linyoujie@hotmail.com>
2020-04-07 12:52:41 -0400

    moving pipes

--- Physics.js
+++ Physics.js
@@ -1,4 +1,5 @@
 import Matter from 'matter-js';
+import Constants from './Constants';
 
 const Physics = (entities,{ touches, time}) => {
     let engine = entities.physics.engine;
@@ -7,6 +8,18 @@ const Physics = (entities,{ touches, time}) => {
     touches.filter(t => t.type ==="press"). forEach(t =>{
         Matter.Body.applyForce(bird, bird.position, {x:0.0, y:-0.1});
     });
+
+    for(let i = 1; i <=4; i++){
+        if (entities["pipe" +i].body.position.x <= -1 * (Constants.PIPE_WIDTH /2)) {
+            Matter.Body.setPosition(entities["pipe"+i].body, {x: Constants.MAX_WIDTH*2 - (Constants.PIPE_WIDTH/2), y: entities["pipe"+i]})
+
+        } else{
+            Matter.Body.translate(entities["pipe"+i].body, {x:-1, y:0})
+        }
+        
+
+
+    }
 
     Matter.Engine.update(engine, time.delta);
 
