95c7a2dfb1fc0a0cafe8abda4b0bb8122d70e08c
linyoujie <linyoujie@hotmail.com>
2020-04-07 12:26:03 -0400

    Initial jumping bird;
    [New] Bird, Wall, Ceiling, Pipe height genetator, UpPipes, calculated DownPipes, physic engine, and touches(press) event added.

---
+++ App.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Constants from './Constants';
+import {GameEngine} from 'react-native-game-engine';
+import Matter from 'matter-js';
+import Bird from './Bird'
+import Wall from './Wall'
+import Physics from './Physics'
+
+export const randomBeteern = (min, max) =>{
+  return Math.floor(Math.random() * (max -min +1) + min);
+  
+}
+
+export const generatePipes = () => {
+  let topPipHeight = randomBeteern(100,(Constants.MAX_HEIGHT/2) -100);
+  let bottomPipHeight = Constants.MAX_HEIGHT - topPipHeight - Constants.GAP_SIZE
+ 
+  let sizes = [topPipHeight, bottomPipHeight]
+
+  if (Math.random() < 0.5){
+    sizes= sizes.reverse();
+  }
+
+  return sizes;
+}
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.GameEngine = null
+    this.entities = this.setupWorld();
+  }
+
+  setupWorld = () => {
+    let engine = Matter.Engine.create ({ enableSleeping: false});
+    let world = engine.world;
+
+    let bird = Matter.Bodies.rectangle(Constants.MAX_WIDTH/4, Constants.MAX_HEIGHT /2, 50,50)
+    let floor = Matter.Bodies.rectangle(Constants.MAX_WIDTH/2, Constants.MAX_HEIGHT -25, Constants.MAX_WIDTH,50, {isStatic: true})
+    let ceiling = Matter.Bodies.rectangle(Constants.MAX_WIDTH/2, 25, Constants.MAX_WIDTH,50, {isStatic: true})
+
+    let [pipe1Height, pipe2Height] = generatePipes();
+    let pipe1 = Matter.Bodies.rectangle( Constants.MAX_WIDTH - (Constants.PIPE_WIDTH / 2), pipe1Height / 2, Constants.PIPE_WIDTH, pipe1Height, { isStatic: true });
+    let pipe2 = Matter.Bodies.rectangle( Constants.MAX_WIDTH - (Constants.PIPE_WIDTH / 2), Constants.MAX_HEIGHT - (pipe2Height / 2), Constants.PIPE_WIDTH, pipe2Height, { isStatic: true });
+
+
+    let [pipe3Height, pipe4Height] = generatePipes();
+    let pipe3 = Matter.Bodies.rectangle( Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH / 2), pipe3Height / 2, Constants.PIPE_WIDTH, pipe3Height, { isStatic: true });
+    let pipe4 = Matter.Bodies.rectangle( Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH / 2), Constants.MAX_HEIGHT - (pipe4Height / 2), Constants.PIPE_WIDTH, pipe4Height, { isStatic: true });
+
+
+    Matter.World.add(world, [bird, floor, ceiling, pipe1,pipe2,pipe3,pipe4]);
+
+
+    return {
+      physics: {engine: engine, world:world},
+      bird:{body: bird, size: [50,50], color: 'red', renderer:Bird},
+      floor:{body: floor, size: [Constants.MAX_WIDTH,50], color: 'black', renderer:Wall},
+      ceiling:{body: ceiling, size: [Constants.MAX_WIDTH,50], color: 'green', renderer:Wall},
+      pipe1:{body: pipe1, size: [Constants.PIPE_WIDTH,pipe1Height], color: 'green', renderer:Wall},
+      pipe2:{body: pipe2, size: [Constants.PIPE_WIDTH,pipe2Height], color: 'green', renderer:Wall},
+      pipe3:{body: pipe3, size: [Constants.PIPE_WIDTH,pipe3Height], color: 'green', renderer:Wall},
+      pipe4:{body: pipe4, size: [Constants.PIPE_WIDTH,pipe4Height], color: 'green', renderer:Wall}
+
+
+    }
+  }
+
+  render() {
+    return(
+    <View style={styles.container}>
+      <GameEngine
+        ref = {(ref) => {this.GameEngine =ref;}}
+        style = {styles.gameContainer}
+        systems = {[Physics]}
+        entities = {this.entities} />
+    </View>
+    )
+  }
+
+
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+
+  },
+
+  gameContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+
+  },
+});
